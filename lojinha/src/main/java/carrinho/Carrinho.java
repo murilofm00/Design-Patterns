182f17177332ef638615adda80991ae0996ba28a
Murilo Fernandes <38534607+murilofm00@users.noreply.github.com>
2020-11-29 15:57:51 -0300

    Singleton Carrinho de compras

---
+++ lojinha/src/main/java/carrinho/Carrinho.java
@@ -0,0 +1,56 @@
+package carrinho;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import item.Item;
+
+public class Carrinho {
+    private static Carrinho instancia;
+
+    private List<Item> itens = new ArrayList<>();
+    private Carrinho(){
+        System.out.println("novoCarrinho");
+    }
+
+    public static Carrinho getInstancia() {
+        if(instancia == null) {
+            instancia = new Carrinho();
+        }
+        return instancia;
+    }
+
+    public void addItem(Item item) {
+        itens.add(item);
+    }
+
+    public float getValor() {
+        float soma = 0;
+        for (Item item:
+             itens) {
+            soma += item.getValor();
+        }
+        return soma;
+    }
+
+    public void gerarNota() {
+        System.out.println("Lojinha");
+        System.out.println("-----------------------------");
+        System.out.println("Produtos");
+        System.out.println("-----------------------------");
+        gerarListaCompra();
+        System.out.println("-----------------------------");
+        System.out.println("Total= \t\t\t\t"+ getValor());
+
+    }
+
+    public void gerarListaCompra() {
+        int posicao = 1;
+        for (Item item:
+                itens) {
+            System.out.println(posicao +". "+ item.getNome()+ "\n\t\t\t\t\t" + item.getValor());
+            posicao++;
+        }
+    }
+
+}
--- lojinha/src/main/java/item/ItemComposto.java
+++ lojinha/src/main/java/item/ItemComposto.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class ItemComposto implements Item{
     private int codigo;
     private String nome;
-    
-    private List<Item> itens = new ArrayList<>();     
-    
+
+    private List<Item> itens = new ArrayList<>();
+
     public ItemComposto(String nome){
         setNome(nome);
     }
@@ -26,13 +26,18 @@ public class ItemComposto implements Item{
         }
         return valor;
     }
-    
+
     public String getNome() {
-        return nome;
+        String _nome = nome +" ( ";
+        for (Item item : itens) {
+            _nome+= item.getNome() +", ";
+        }
+        _nome = _nome.substring(0, _nome.length() -2) + " )";
+        return _nome;
     }
 
     private void setNome(String nome) {
         this.nome = nome;
     }
-    
+
 }
